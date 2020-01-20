ad630178fea994db3aa523f1ff7d9e0c80079608
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-01-21 03:48:57 +1300

    add build folder

---
+++ build/precache-manifest.b64b5f50ffd008ef06ba24a8100c195e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418aa661c7cd346c6a2947e823cb6d6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e011b3976f28d9c17815",
+    "url": "/static/css/main.8c5db658.chunk.css"
+  },
+  {
+    "revision": "f13f144e18891c73c356",
+    "url": "/static/js/2.8bb9fa26.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/static/js/2.8bb9fa26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e011b3976f28d9c17815",
+    "url": "/static/js/main.fc83a0f9.chunk.js"
+  },
+  {
+    "revision": "42a8b147aa10dbde66ec",
+    "url": "/static/js/runtime-main.9f7c15aa.js"
+  }
+]);
